aef0c604e8b9f6e9fefe44671a3a5902c12a3588
jeffbai996 <j.bai@wustl.edu>
2022-11-04 09:47:10 -0700

    Updated import_code_block.js - using * to import everything

--- import_code_block.js
+++ import_code_block.js
@@ -14,3 +14,13 @@ import { uppercaseString, lowercaseString } from './string_functions.js'
 
 uppercaseString("hello");
 lowercaseString("WORLD!");
+
+
+// importing everything in a file
+
+
+import * as stringFunctions from './string_functions.js'
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
