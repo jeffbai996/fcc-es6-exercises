67cc8ca4ee1fe40c8446a387582ac02a8c060cb3
jeffbai996 <j.bai@wustl.edu>
2022-11-04 09:35:16 -0700

    Completed getter and setter exercise

--- getters_setters.js
+++ getters_setters.js
@@ -43,7 +43,19 @@
 // In other words, you are abstracting implementation details from the user.
 
 // Only change code below this line
+class Thermostat {
+  constructor(ftemp) {
+    this.ftemp = ftemp;
+  }
 
+  get temperature() {
+    return (5/9) * (this.ftemp - 32);
+  }
+
+  set temperature(ctemp) {
+    this.ftemp = (ctemp * 9.0) / 5 + 32
+  }
+}
 // Only change code above this line
 
 const thermos = new Thermostat(76); // Setting in Fahrenheit scale
